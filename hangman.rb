3d556e9fa493b7ae8f7af05ac3bcf736c6ece8c7
Ash-x1 <ash91p@proton.me>
2026-01-01 18:40:08 +0100

    Add loading from txt file with condition

--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,19 @@
 #!/usr/bin/env ruby
-
+# require 'yaml'
 class Hangman
+  attr_reader :secret, :guess
 
-  
+  def initalize(secret, guess)
+    @secret = secret
+    @guess  = guess
+  end
+
+  def pick_word
+    file = File.readlines('hangman_words.txt').map(&:chomp)
+    word = file.select { |word| 5 <= word.length && word.length <= 12 }.sample
+    p word
+  end
 end
+
+play = Hangman.new
+play.pick_word
